84d9a9ba932cd80b11d1bb96db443e88197fe023
fabianpid <fabianpid34@gmail.com>
2026-04-24 19:04:12 -0600

    Add files via upload

--- conexion.php
+++ conexion.php
@@ -3,22 +3,11 @@ $host = $_ENV['MYSQLHOST'];
 $port = $_ENV['MYSQLPORT'];
 $user = $_ENV['MYSQLUSER'];
 $pass = $_ENV['MYSQLPASSWORD'];
-$db   = $_ENV['MYSQLDATABASE'];
+$db   = $_ENV['MYSQL_DATABASE'];
 
 $con = mysqli_connect($host, $user, $pass, $db, $port);
 
 if (!$con) {
-    // Si falla, intentamos con $_SERVER por si acaso
-    $host = $_SERVER['MYSQLHOST'];
-    $port = $_SERVER['MYSQLPORT'];
-    $user = $_SERVER['MYSQLUSER'];
-    $pass = $_SERVER['MYSQLPASSWORD'];
-    $db   = $_SERVER['MYSQLDATABASE'];
-    
-    $con = mysqli_connect($host, $user, $pass, $db, $port);
-}
-
-if (!$con) {
-    die("Error fatal de conexión: " . mysqli_connect_error());
+    die("Error de conexión: " . mysqli_connect_error());
 }
 ?>
